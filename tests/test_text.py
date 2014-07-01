ac64961211260796c3a3d138b85fe0b944bef25f
Zack Maril <zmaril@sunlightfoundation.com>
2014-07-01 16:11:50 +0000

    Only strip acronyms now, not match on them

--- tests/test_text.py
+++ tests/test_text.py
@@ -12,8 +12,8 @@ cases = [(["google"],"Google inc"),
          (['international buddhism sangha association', 'master wan ko yee'],
           "Intl. Buddhism Sangha Assn. (FKA Dr. David Wu on behalf of Master Wan Ko Yee)"),
 
-         (["dca", "dredging contractors of america"],"DREDGING CONTRACTORS OF AMERICA (DCA)"), #DCA
-         (["hart", "housing action resource trust"],"housing action resource trust (\"hart\")"), #DCA         
+         (["dredging contractors of america"],"DREDGING CONTRACTORS OF AMERICA (DCA)"), #DCA
+         ([ "housing action resource trust"],"housing action resource trust (\"hart\")"), #DCA         
 
          (["orange broadband holding"],"orange broadband holding company")  #dba
 ]
